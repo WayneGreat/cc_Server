e51cbd8296dc72babcf3b45ad8288c090596b3f3
wayne <weihongye669@gmail.com>
2023-04-15 12:31:36 +0800

    add mutli_io.c

---
+++ mutli_io.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
+int main() {
+    //创建服务器fd
+    //@AF_INET ipv4协议
+    //@SOCK_SREAM 字节流
+    int socketFd = socket(AF_INET, SOCK_STREAM, 0); //io
+
+    //设置需监听的地址、端口
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); //0.0.0.0
+    serverAddr.sin_port = htons(9999);
+
+    //绑定地址端口到fd
+    if (-1 == bind(socketFd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr_in))) {
+        printf("bind failed: %s", strerror(errno));
+        return -1;
+    }
+
+    //监听地址端口
+    listen(socketFd, 10);
+
+};
