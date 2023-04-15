b45c4a675af77031d66b78a455e9d7235df8b2f2
wayne <weihongye669@gmail.com>
2023-04-15 19:32:20 +0800

    add select,poll

--- multi_io.c
+++ multi_io.c
@@ -5,7 +5,34 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/select.h>
+#include <sys/poll.h>
 
+#define BUFFER_LENGTH 1024
+
+#define POLL_SIZE 1024
+
+// 线程执行函数
+void *client_thread(void *arg) {
+    int clientFd = *(int*)arg;
+
+    while (1)
+    {
+        char buffer[BUFFER_LENGTH] = {0};
+        int ret = recv(clientFd, buffer, BUFFER_LENGTH, 0);
+        if (ret == 0) {
+            // 客户端断开处理
+            close(clientFd);
+            break;
+        }
+
+        printf("ret: %d, buffer: %s\n", ret, buffer);
+
+        send(clientFd, buffer, ret, 0);
+    }
+}
 
 int main() {
     //创建服务器fd
@@ -28,21 +55,135 @@ int main() {
     //监听地址端口
     listen(socketFd, 10);
 
-#if 1
+    // sleep(10);
 
-#if 1
-
-
+#if 0
+    // 设置socketFd为非阻塞
+    int flags = fcntl(socketFd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    fcntl(socketFd, F_SETFL, flags);
 #endif
 
     //为客户端建立新连接
     struct sockaddr_in clientAddr;
     socklen_t len = sizeof(clientAddr);
-    int clientFd = accept(socketFd, (struct sockaddr *)&clientAddr, &len); // block: 等待一个客户端连接
+
+#if 0
+
+    while (1) {
+        int clientFd = accept(socketFd, (struct sockaddr *)&clientAddr, &len); // block: 等待一个客户端连接
+
+        // printf("clientFd: %d, socketFd: %d\n", clientFd, socketFd);
+
+#if 0
+    char buffer[BUFFER_LENGTH] = {0};
+    int ret = recv(clientFd, buffer, BUFFER_LENGTH, 0);
+
+    printf("ret: %d, buffer: %s\n", ret, buffer);
+
+    send(clientFd, buffer, ret, 0);
+#else
+
+        pthread_t threadId;
+        pthread_create(&threadId, NULL, client_thread, &clientFd);
+    }
 #endif
 
-    getchar(); // block
+#elif 0 //select
 
-    getchar();
+    // select(maxfd, rfds, wfds, efds, timeout);
+    fd_set rfds, rset;
+    FD_ZERO(&rfds);
+    FD_SET(socketFd, &rfds);
 
-};
+    int maxfd = socketFd;
+    int clientFd = 0;
+
+    while (1) {
+        rset = rfds;
+
+        int nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+
+        printf("select event\n");
+
+        //检测bit图的下标为socketFd是否为1
+        if (FD_ISSET(socketFd, &rset)) {
+            clientFd = accept(socketFd, (struct sockaddr *)&clientAddr, &len);
+            printf("accept: %d\n", clientFd);
+
+            FD_SET(clientFd, &rfds);
+            if (clientFd > maxfd) maxfd = clientFd;
+
+            if (-- nready == 0) continue;
+        }
+
+        int i = 0;
+        for (i = socketFd+1; i <= maxfd; i++) {
+            if (FD_ISSET(i, &rset)) {
+                char buffer[BUFFER_LENGTH] = {0};
+                int ret = recv(clientFd, buffer, BUFFER_LENGTH, 0);
+                if (ret == 0)
+                {
+                    // 客户端断开处理
+                    close(clientFd);
+                    break;
+                }
+
+                printf("ret: %d, buffer: %s\n", ret, buffer);
+
+                send(clientFd, buffer, ret, 0);
+            }
+        }
+    }
+#else
+
+    struct pollfd fds[POLL_SIZE] = {0};
+
+    fds[socketFd].fd = socketFd;
+    fds[socketFd].events = POLLIN; // events: 需要监听的事件
+
+    int maxfd = socketFd;
+    int clientFd = 0;
+
+    while (1) {
+        int nready = poll(fds, maxfd+1, -1);
+
+        // revents: 就绪事件
+        // 监听fd就绪
+        if (fds[socketFd].revents & POLLIN) {
+            clientFd = accept(socketFd, (struct sockaddr *)&clientAddr, &len);
+            printf("accept: %d\n", clientFd);
+
+            fds[clientFd].fd = clientFd;
+            fds[clientFd].events = POLLIN;
+
+            if (clientFd > maxfd) maxfd = clientFd;
+
+            if (-- nready == 0) continue;
+        }
+
+        // 其他fd就绪
+        int i = 0;
+        for (i = socketFd+1; i <= maxfd; i++) {
+            if (fds[i].revents & POLLIN) {
+                char buffer[BUFFER_LENGTH] = {0};
+                int ret = recv(i, buffer, BUFFER_LENGTH, 0);
+                if (ret == 0)
+                {
+                    // 客户端断开处理
+                    fds[i].fd = -1;
+                    fds[i].events = 0;
+                    close(i);
+                    break;
+                }
+
+                printf("ret: %d, buffer: %s\n", ret, buffer);
+
+                send(i, buffer, ret, 0);
+            }
+        }
+    }
+
+#endif
+
+}
